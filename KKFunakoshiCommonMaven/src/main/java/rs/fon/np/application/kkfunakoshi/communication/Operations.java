4c521fab2d16a5584dd5e663e2a23d3707b4f45f
jelena <repac01jelena@gmail.com>
2023-04-24 22:31:33 +0200

    Added javaDoc for class Operations.

--- KKFunakoshiCommonMaven/src/main/java/rs/fon/np/application/kkfunakoshi/communication/Operations.java
+++ KKFunakoshiCommonMaven/src/main/java/rs/fon/np/application/kkfunakoshi/communication/Operations.java
@@ -5,30 +5,95 @@
 package rs.fon.np.application.kkfunakoshi.communication;
 
 /**
- *
- * @author Jeks
+ * Klasa koja predstavlja listu operacija.
+ * Svaka operacija je predstavljena tipom integer.
+ * Konstante se koriste kako bi se olakšala komunikacija između različitih delova programa koji se bave različitim funkcijama.
+ * @author Jelena Repac
  */
 public class Operations {
+	/**
+	 * Prijava na sistem.
+	 */
     public static final int LOGIN=1;
+    /**
+     * Vraca sve clanove kluba.
+     */
     public static final int GET_ALL_MEMBERS=2;
+    /**
+     * Vraca sve clanarine clanova.
+     */
     public static final int GET_ALL_MEMBERSHIPFEES=3;
+    /**
+     * Dodaje clana.
+     */
     public static final int ADD_MEMBER=4;
+    /**
+     * Vraca listu gradova.
+     */
     public static final int GET_CITIES=5;
+    /**
+     * Odjava sa sistema.
+     */
     public static final int LOGOUT=6;
+    /**
+     * Server zaustavljen.
+     */
     public static final int SERVER_STOPPED=7;
+    /**
+     * Za prekidanje veze sa serverom.
+     */
     public static final int STOP=8;
+    /**
+     * Vraca clanove na osnovu upita.
+     */
     public static final int GET_MEMBERS_BY_QUERY=9;
+    /**
+     * Brise clana.
+     */
     public static final int DELETE_MEMBER=10;
+    /**
+     * Azurira podatke clana.
+     */
     public static final int UPDATE_MEMBER=11;
+    /**
+     * Dodaje takmicenje.
+     */
     public static final int ADD_COMPETITION=12;
+    /**
+     * Vraca listu svih takmicenja.
+     */
     public static final int GET_ALL_COMPETITIONS=13;
+    /**
+     * Dodaje tim.
+     */
     public static final int ADD_TEAM=14;
+    /**
+     * Vraca listu svih timova.
+     */
     public static final int GET_ALL_TEAMS=15;
+    /**
+     * Dodaje rezultat ostvaren na takmicenju.
+     */
     public static final int ADD_RESULT=16;
+    /**
+     * Vraca sve rezultate na osnovu upita.
+     */
     public static final int GET_RESULTS_BY_QUERY=17;
+    /**
+     * Azurira medalje clana.
+     */
     public static final int UPDATE_MEMBER_MEDALS=18;
+    /**
+     * Azurira rezultat.
+     */
     public static final int UPDATE_RESULT=19;
+    /**
+     * Azurira rezultat tima.
+     */
     public static final int UPDATE_RESULT_TEAM=20;
+    /**
+     * Vraca sva takmicanja na osnovu upita.
+     */
     public static final int GET_COMPETITIONS_BY_QUERY=21;
     
 }
